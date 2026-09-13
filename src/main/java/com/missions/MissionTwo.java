bb2531ce773bf750bfe9c186d8bab19200417185
Juan Pablo Alzate <jxxnpx@gmail.com>
2026-09-13 17:27:06 -0500

    feat: mission two logic implemented (to be improved)

--- src/main/java/com/missions/MissionTwo.java
+++ src/main/java/com/missions/MissionTwo.java
@@ -1,4 +1,39 @@
 package main.java.com.missions;
 
+import main.java.com.structures.Djikstra;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MissionTwo {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(sc.nextLine());
+        int N, C;
+        int[] NCSD;
+        for (int i = 0; i < T; i++) {
+            NCSD = Arrays.stream(sc.nextLine().trim().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            N = NCSD[0];
+            Djikstra d = new Djikstra(N);
+            C = NCSD[1];
+            int A, B, W;
+            int[] ABW;
+            for (int j = 0; j < C; j++) {
+                ABW = Arrays.stream(sc.nextLine().trim().split("\\s+"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                A = ABW[0];
+                B = ABW[1];
+                W = ABW[2];
+                d.addEdge(A, B, W);
+            }
+            if(d.dijkstra(NCSD[2], NCSD[3]) != Integer.MAX_VALUE){
+                System.out.printf("Case #%d: %d\n", (i+1), d.dijkstra(NCSD[2], NCSD[3]));
+            } else {
+                System.out.printf("Case #%d: Nina is very sad\n", (i+1));
+            }
+        }
+    }
 }
--- src/main/java/com/structures/Djikstra.java
+++ src/main/java/com/structures/Djikstra.java
@@ -1,4 +1,76 @@
 package main.java.com.structures;
 
+import java.util.*;
+
 public class Djikstra {
+
+    private static class Edge {
+        int target;
+        int weight;
+
+        public Edge(int target, int weight) {
+            this.weight = weight;
+            this.target = target;
+        }
+    }
+
+    int V;
+    Map<Integer, List<Edge>> adj;
+
+    public Djikstra(int v) {
+        V = v;
+        adj = new HashMap<>();
+    }
+
+    public void addEdge(int A, int B, int W){
+        addConnection(B, A, W);
+        addConnection(A, B, W);
+    }
+
+    private void addConnection(int A, int B, int W) {
+        if(adj.containsKey(B)){
+            boolean found = false;
+            for (Edge e : adj.get(B)){
+                if(e.target == A){
+                    e.weight = Math.min(W, e.weight);
+                    found = true;
+                }
+            }
+            if(!found) adj.get(B).add(new Edge(A, W));
+        } else {
+            adj.put(B, new ArrayList<>(List.of(new Edge(A, W))));
+        }
+    }
+
+    public int dijkstra(int start, int end) {
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        // Min-heap: ordena por distancia
+        PriorityQueue<int[]> pq =
+                new PriorityQueue<>(
+                        Comparator.comparingInt(a -> a[1])
+                );
+        pq.add(new int[]{start, 0});
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int u = curr[0], d = curr[1];
+
+            // Obsoleto si ya hay mejor camino
+            if (d > dist[u]) continue;
+
+            // RELAJACIÓN de aristas
+            for (Edge e : adj.getOrDefault(u, List.of())) {
+                if (dist[u] + e.weight < dist[e.target]) {
+                    dist[e.target] = dist[u] + e.weight;
+                    pq.add(new int[]{
+                            e.target, dist[e.target]
+                    });
+                }
+            }
+        }
+        return dist[end];
+    }
 }
